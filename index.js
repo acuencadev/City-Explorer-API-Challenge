54297e236925c4be2b304c1cebac8be12cbcfbbc
Amador Cuenca <sphi02ac@gmail.com>
2024-01-04 10:44:52 -0300

    feat: Create the API prototype

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("Hello, world!");
+const express = require('express');
+const app = express();
+
+// Supermarkets endpoint
+app.get('/api/supermarkets', (req, res) => {
+  res.send('Supermarkets data goes here');
+});
+
+// Entertainment/Attractions endpoint
+app.get('/api/entertainment', (req, res) => {
+  res.send('Entertainment data goes here');
+});
+
+// Parks endpoint
+app.get('/api/parks', (req, res) => {
+  res.send('Parks data goes here');
+});
+
+// Restaurants endpoint
+app.get('/api/restaurants', (req, res) => {
+  res.send('Restaurants data goes here');
+});
+
+// User Feedback endpoint
+app.post('/api/feedback', (req, res) => {
+  res.send('Feedback received');
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
