660f1746c0d725f90f192c486b9aaabdab783132
Amador Cuenca <sphi02ac@gmail.com>
2024-01-05 10:58:35 -0300

    feat: Parse the JSON content convert it to a Location pojo

--- index.js
+++ index.js
@@ -11,7 +11,29 @@ let feedback = [];
 async function fetchData(category, rect, limit) {
   const url = `https://api.geoapify.com/v2/places?categories=${category}&filter=rect:${rect}&limit=${limit}&apiKey=${process.env.API_KEY}`;
   const response = await axios.get(url);
-  return response.data;
+  
+  const locations = response.data.features.map(feature => {
+    const { properties } = feature;
+    const name = properties.name;
+    const address = properties.formatted;
+    const lat = properties.lat;
+    const lon = properties.lon;
+    const categories = properties.categories;
+    
+    return new Location(name, address, lat, lon, categories);
+  });
+  
+  return locations;
+}
+
+class Location {
+  constructor(name, address, lat, lon, categories) {
+    this.name = name;
+    this.address = address;
+    this.lat = lat;
+    this.lon = lon;
+    this.categories = categories;
+  }
 }
 
 // Supermarkets endpoint
